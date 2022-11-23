19cb23c7b6503cb429a261b026ee17e958b1e550
Kauã Ambrosio <116666437+Kraezper@users.noreply.github.com>
2022-11-23 11:12:33 -0300

    Aula 1

---
+++ teste.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+var n = require('./primeiromodulo')
+
+var dia = require('./moduloData')
+
+http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write('<b>Aula de Node.js<b> <br>');
+    res.write('<i> </i> ');
+    res.write(n.nome()+ '<br>')
+    res.write(dia.diaDaSemana()+ '<br>');
+    res.end('Bom dia!');
+
+}).listen(80);
